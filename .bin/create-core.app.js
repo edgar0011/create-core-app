68f825d07bed5d06b746efd774975c5bc5832386
Martin Weiser  (Inventi) <martin.weiser@inventi.cz>
2021-09-11 02:26:57 +0200

    bin command added

---
+++ .bin/create-core.app.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const { spawn } = require("child_process");
+const fs = require("fs");
+
+let folderName = '.';
+
+if (process.argv.length >= 3) {
+  folderName = process.argv[2];
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+}
+
+const clone = spawn("git", ["clone", "https://github.com/edga0011/create-core-app.git", folderName]);
+
+clone.on("close", code => {
+  if (code !== 0) {
+    console.error("Cloning the template failed!")
+    process.exit(code);
+  } else {
+    console.log("🦀 Core + 🕸 App = ❤");
+  }
+});
